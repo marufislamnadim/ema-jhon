9d127571f26f798adba9b276c128b5a88e1523b0
marufislamnadim <marufislamnadim@gmail.com>
2023-03-29 08:29:37 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt='ema-jhon-commerz-logo'></img>
+            <div>
+                <a href="/Shop">Shop</a>
+                <a href="/Order">Order</a>
+                <a href="/Inventory Management">Inventory Management</a>
+                <a href="/login">login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
