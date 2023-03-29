fb08a30827840f2761953eefed1f44bf522485cc
marufislamnadim <marufislamnadim@gmail.com>
2023-03-29 10:23:36 +0600

    products added and number of selected items are shown in the shopping cart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Product.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Product = (props) => {
-    const { img, name, ratings, seller, price } = props.product;
+    const { img, name, ratings, seller, price} = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className="product">
             <img src={img} alt="" />
@@ -12,7 +16,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating {ratings} Star</p>
             </div>
-            <button className="btn-cart">Add To Cart</button>
+            <button onClick={()=>handleAddToCart(props.product)} className="btn-cart">Add To Cart  <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };
